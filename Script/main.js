cdadca01163a64ec9318535b00feffd9d8eeadc3
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2024-12-24 10:39:44 +0100

    Update the current year in the footer section to get the current year with JavaScript

--- Script/main.js
+++ Script/main.js
@@ -1,3 +1,7 @@
 import { resturants } from "./featured_resturant.js";
-import {foods} from './popular_foods.js';
-import {howItWorks} from './how_it_works.js';
+import { foods } from './popular_foods.js';
+import { howItWorks } from './how_it_works.js';
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+document.querySelector('.year').innerHTML = currentYear;
